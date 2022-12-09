8a6dce8d0b86d6ba68243f9f229c39804f72ac85
54padmakshi <115454581+54padmakshi@users.noreply.github.com>
2022-12-09 19:54:58 +0530

    update

--- script.js
+++ script.js
@@ -1,38 +1,61 @@
 //your code here
 
-let userData ; //creating an object to call the functions
-
-let age =document.querySelector('.age') 
-let email=document.getElementsByClassName('email')
-let phone = document.getElementsByClassName('phone')
-
-let info =document.getElementsByClassName('info')   // creating a div giving the classname in HTML to display the message
  
-let name1 = document.querySelector(".name") // we need to display the name by default
-let lastn1 = document.getElementsByClassName("lastn") // we need to display lastname by default
-let image = document.getElementsByClassName("pic") // we need to display pic by default
+let name1 = document.querySelector("#name"); // we need to display the name by default
+let image = document.querySelector(".pic"); // we need to display pic by default
+let headingadd =document.querySelector(".addinfo");
+let btngrp =document.querySelector(".btngrp");
+let agebtn =document.querySelector('[data-attr="age"]');
+let emailbtn =document.querySelector('[data-attr="email"]');
+let phonebtn =document.querySelector('[data-attr="phone"]'); 
+let additionalInfo = document.querySelector(".additionalinfo");
+let getuser =document.querySelector("#getUser");
+// 1.fetching the url
 
-fetch('https://randomuser.me/api/')
+const url ="https://randomuser.me/api/";
+let user = {}; // taking an empty variable to get all the data
 
- .then(response => response.json() ) 
- .then(result =>{ userData = result;
-    name1.innerHTML =result.results[0].name.first + "       " + result.results[0].name.last + "        "+ result.results[0].picture.large 
-    // displaying name and last name and image
-  
-} )
- 
- 
- function myFunction1() { 
-  
-    info[0].innerHTML = userData.results[0].dob.age
- }
- function myFunction2() {
-    info[0].innerHTML = userData.results[0].email
- }
- function myFunction3() {
-    info[0].innerHTML = userData.results[0].phone
- }
- 
+const getData = async ()=>{
+   try {
+      const result = await fetch(url);
+      const data = await result.json();
+      user = data.results[0];
+      console.log(user);
+   } catch (error) {
+      alert (error);
+      console.log(error);
+    }
+}
+//getData();
 
+const displayData = async()=>{
+   await getData();
+   name1.textContent = `Name : ${user.name.title}  ${user.name.first} ${user.name.last}`;
+   image.src=user.picture.large;
+   btngrp.style.display = "none";
+   additionalInfo.textContent = "";
+} 
+const toggleAdditionalInfo =()=>{
+   if(btngrp.style.display === "none"){
+      btngrp.style.display = "block";
+   }else{
+      btngrp.style.display = "none";
+   }
+}
+const showAdditionalInfo = (e)=>{
+   const att = e.target.getAttribute("data-attr");
+   if (att == "age") {
+     additionalInfo.textContent = user.dob.age;
+   } else if (att == "phone") {
+     additionalInfo.textContent = user.phone;
+   } else if (att == "email") {
+     additionalInfo.textContent = user.email;
+   }
+}
+displayData();
 
-
+headingadd.addEventListener("click",toggleAdditionalInfo);
+agebtn.addEventListener("click",showAdditionalInfo);
+emailbtn.addEventListener("click",showAdditionalInfo);
+phonebtn.addEventListener("click",showAdditionalInfo);
+getuser.addEventListener("click",displayData);
